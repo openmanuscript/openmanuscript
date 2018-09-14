e130076f438d42e49ccee3d5d7e0d98414fddd69
David Rogers <davidhrogers@me.com>
2018-09-14 07:58:45 -0600

    updating setup file

--- tools/setup.py
+++ tools/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/moonimal/openmanuscript",
+    url="https://github.com/openmanuscript/openmanuscript",
     packages=setuptools.find_packages(),
     scripts=["bin/openms"],
     classifiers=[
