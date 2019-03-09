7ebc32732b670ac95b32f9f998ad57b098a572d7
David H. Rogers <david@dhrogers.com>
2019-03-09 15:24:03 -0700

    adding to docx creation

--- tools/oms/reader/__init__.py
+++ tools/oms/reader/__init__.py
@@ -1,14 +1,48 @@
+import os
+import json
+
 READER = {
-    "oms" : "none"
+    "oms" : "none",
+    "authorfile" : "none",
+    "manuscriptfile" : "none"
+}
+
+AUTHOR = {
+}
+
+MANUSCRIPT = {
 }
 
 def set_database( db ):
     global READER
 
     READER["db"] = db
+    READER["authorfile"] = os.path.join( db, "author.json" )
+    READER["manuscriptfile"] = os.path.join( db, "manuscript.json" )
 
-def read( omsdb ):
-    global READER
+def get_authorfile():
+    return READER["authorfile"]
 
-    set_database(omsdb)
-    print(omsdb)
+def get_manuscriptfile():
+    return READER["manuscriptfile"]
+
+def get_author():
+    global AUTHOR
+    return AUTHOR
+
+def get_manuscript():
+    global MANUSCRIPT
+    return MANUSCRIPT
+
+def read():
+    global AUTHOR
+    global MANUSCRIPT
+    
+    with open( get_authorfile() ) as author_file:
+        AUTHOR = json.load( author_file )
+        AUTHOR = AUTHOR["author"]
+
+    with open( get_manuscriptfile() ) as manuscript_file:
+        MANUSCRIPT = json.load( manuscript_file )
+        MANUSCRIPT = MANUSCRIPT["manuscript"]
+        
