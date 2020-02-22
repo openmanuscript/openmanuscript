b9ed8f5dfb7d0ab4d7fd2f43c2809cd2b2a0a740
moonimal <moonimal@hotmail.com>
2020-02-22 11:25:19 -0700

    adding markdown outline tool

--- tools/setup.py
+++ tools/setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/openmanuscript/openmanuscript",
     packages=["openms", "openms.html"],
-    scripts=["oms", "oms2outline"],
+    scripts=["oms", "oms2outline", "oms2md"],
     install_requires=[
         "markdown",
         "python-docx"
