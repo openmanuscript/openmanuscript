571bd51c5dfb27e30becd77abe1458c1e3bc54e7
moonimal <moonimal@hotmail.com>
2020-08-17 14:16:37 -0600

    upping version number

--- src/openms/core.py
+++ src/openms/core.py
@@ -7,7 +7,7 @@ import yaml
 
 __oms = {
     "name"        : "OpenManuscript",
-    "toolversion" : "3.5",
+    "toolversion" : "3.5.1",
     "specversion" : "2.0"
 }
 
--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="openms",
-    version="3.5",
+    version="3.5.1",
     author="David H. Rogers",
     author_email="david@dhrogers.com",
     description="A minimal implementation of OpenManuscript capabilities.",
